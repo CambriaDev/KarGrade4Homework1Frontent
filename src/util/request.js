bae6a0418353d0af69d92a98c72ec461f82dca41
Cambria <i@cambria.cam>
2023-10-13 19:21:59 +0800

    init

--- src/util/request.js
+++ src/util/request.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const service = axios.create({
   // baseURL: process.env.VUE_APP_BASE_API,
-  baseURL: 'http://192.168.128.2:8080/',
+  baseURL: 'http://kar.sylu.asia/api/',
   timeout: 5000
 })
 
